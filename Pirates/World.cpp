796baacea3453629d1853532a098733f8036e494
Anatoliy <anatoliy.svyrydenko@gmail.com>
2019-11-17 13:43:47 +0200

    - init world with predefined map useful for testing path search

--- Pirates/World.cpp
+++ Pirates/World.cpp
@@ -19,11 +19,13 @@ World::World() :
 {
 	//srand(unsigned(std::time(0)));
 
-	pirate.x = 2;
-	pirate.y = 2;
+    pirate.x = 2;
+    pirate.y = 1;
 
-    treasure.x = random(1, sizeX - 2);
-    treasure.y = random(1, sizeY - 2);
+//    treasure.x = random(1, sizeX - 2);
+//    treasure.y = random(1, sizeY - 2);
+    treasure.x = 6;
+    treasure.y = 1;
 
     enemyPirate.x = random(1, sizeX - 2);
     enemyPirate.y = random(1, sizeY - 2);
@@ -46,18 +48,50 @@ void World::SetCell(size_t row, size_t col, char value) {
 
 void World::initGameField()
 {
-    gameField[0] = string(sizeX, CELL_WALL);
-    gameField[sizeY - 1] = string(sizeX, CELL_WALL);
+    gameField[0] =  "******************************";
+    gameField[1] =  "* @ * x    *                 *";
+    gameField[2] =  "*   *      *                 *";
+    gameField[3] =  "*          *                 *";
+    gameField[4] =  "*          *          *      *";
+    gameField[5] =  "*          *          *      *";
+    gameField[6] =  "*          ************      *";
+    gameField[7] =  "*                            *";
+    gameField[8] =  "*                            *";
+    gameField[9] =  "*                            *";
+    gameField[10] = "*                            *";
+    gameField[11] = "*                            *";
+    gameField[12] = "*                            *";
+    gameField[13] = "*                            *";
+    gameField[14] = "*                            *";
+    gameField[15] = "*                            *";
+    gameField[16] = "*                            *";
+    gameField[17] = "*                            *";
+    gameField[18] = "*                            *";
+    gameField[19] = "*                            *";
+    gameField[20] = "*                            *";
+    gameField[21] = "*                            *";
+    gameField[22] = "*                            *";
+    gameField[23] = "*                            *";
+    gameField[24] = "*                            *";
+    gameField[25] = "*                            *";
+    gameField[26] = "*                            *";
+    gameField[27] = "*                            *";
+    gameField[28] = "*                            *";
+    gameField[29] = "******************************";
 
-    for (size_t i = 1; i < sizeY - 1; ++i) {
-        gameField[i] = string(sizeX, CELL_EMPTY);
-        gameField[i][0] = CELL_WALL;
-        gameField[i][sizeX - 1] = CELL_WALL;
-    }
 
-    gameField[pirate.y][pirate.x] = '@';
-    gameField[enemyPirate.y][enemyPirate.x] = '&';
-    gameField[treasure.y][treasure.x] = 'x';
+//    gameField[0] = string(sizeX, CELL_WALL);
+//    gameField[sizeY - 1] = string(sizeX, CELL_WALL);
+
+//    for (size_t i = 1; i < sizeY - 1; ++i) {
+//        gameField[i] = string(sizeX, CELL_EMPTY);
+//        gameField[i][0] = CELL_WALL;
+//        gameField[i][sizeX - 1] = CELL_WALL;
+//    }
+
+//    gameField[pirate.y][pirate.x] = '@';
+//    gameField[enemyPirate.y][enemyPirate.x] = '&';
+//    gameField[treasure.y][treasure.x] = 'x';
 }
 
 bool World::CheckWin()
